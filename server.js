67b8a36ff6b868a3b4d6d8e89e55947325170fb6
Mahuta <mahutahs@yahoo.co.nz>
2016-04-21 02:20:37 +1200

    got JS up and running- need css help with hbs and someone to double check my code

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ app.engine('handlebars', exphbs({defaultLayout: ''}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.get('/search', function(req, res){
   var data = {};
   var api_key = '0502b72a-4912-4b5e-b71e-5ee60617b694';
-  var s_toSearch = 'deathgun';
-  var URL = 'https://oce.api.pvp.net/api/lol/oce/v1.4/summoner/by-name/' + s_toSearch + '?api_key=' + api_key;
+  var s_toSearch = req.query.summoner.toLowerCase();
+  
 
 async.waterfall([
 	function(callback) {
+		var URL = 'https://oce.api.pvp.net/api/lol/oce/v1.4/summoner/by-name/' + s_toSearch + '?api_key=' + api_key;
 		request(URL, function(err, response, body){
 			if(!err && response.statusCode == 200) {
 				var json = JSON.parse(body);
@@ -28,13 +33,47 @@ async.waterfall([
 			}
 		});
 	},
-],
-function(err,data) {
-	if(err) {
-		console.log(err);
-		return;
+	function(data, callback) {
+		var URL = 'https://oce.api.pvp.net/api/lol/oce/v1.3/stats/by-summoner/' + data.id + '/ranked?season=SEASON2016&api_key=' + api_key
+		request(URL, function(err, response, body){
+			if(!err && response.statusCode == 200) {
+				var json = JSON.parse(body);
+				var mostPlayed = 0;
+				var championPlayed = 0;
+				for (var c = 0;c < json['champions'].length; c++){
+					var currentPlayed = json['champions'][c]['stats'].totalSessionsPlayed;
+					if(currentPlayed > mostPlayed && !json['champions'][c].id == 0) {
+						mostPlayed = currentPlayed;
+						json['champions'][c].id;
+					}
+				}
+				console.log(mostPlayed)
+				callback(null, data)
+			}else{
+				console.log('line 53')
+			}
+		});
+	},
+	function(data, callback) {
+		var URL = 'https://global.api.pvp.net/api/lol/static-data/oce/v1.2/champion/' + data.champId + '?locale=en_US&champData=infor&api_key=' + api_key
+		request (URL, function(err,response, body){
+			if(response.statusCode == 200) {
+				var json = JSON.parse(body);
+				data.champName = json['name'];
+				console.log(data.champName);
+				callback(null, data);
+			} else {
+				console.log(err);
+			}
+		});
 	}
-
+],
+	function(err,data) {
+		if(err) {
+			console.log(err);
+			return;
+	}
+	console.log(data)
 	res.render('index', {
 		info: data
 	});
@@ -44,3 +83,4 @@ function(err,data) {
 
 var port = Number(process.env.PORT || 3000);
 app.listen(port);
+
