a568f98c0f0180eb7e0d8047d8fa0987a53404b2
Mahuta <mahutahs@yahoo.co.nz>
2016-04-21 12:40:44 +1200

    hbs css working- body not appending to website

--- server.js
+++ server.js
@@ -4,41 +4,42 @@ var exphbs = require('express-handlebars');
 var request = require('request');
 var async = require('async')
 /*var lol = require('/lol.js');*/
-
-app.engine('handlebars', exphbs({defaultLayout: ''}));
-
 app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+
+
+app.use(express.static('./views'))
 
 app.get('/', function(req, res){
-  var data = {};
-  var api_key = '0502b72a-4912-4b5e-b71e-5ee60617b694';
-  var s_toSearch = 'deathgun';
-  var URL = 'https://oce.api.pvp.net/api/lol/oce/v1.4/summoner/by-name/' + s_toSearch + '?api_key=' + api_key;
+	res.render('index');
+//   var data = {};
+//   var api_key = '0502b72a-4912-4b5e-b71e-5ee60617b694';
+//   var s_toSearch = 'deathgun';
+//   var URL = 'https://oce.api.pvp.net/api/lol/oce/v1.4/summoner/by-name/' + s_toSearch + '?api_key=' + api_key;
 
-async.waterfall([
-	function(callback) {
-		request(URL, function(err, response, body){
-			if(!err && response.statusCode == 200) {
-				var json = JSON.parse(body);
-				data.id = json[s_toSearch].id;
-				data.name = json[s_toSearch].name;
-				callback(null, data);
-			} else {
-				console.log(err);
-			}
-		});
-	},
-],
-function(err,data) {
-	if(err) {
-		console.log(err);
-		return;
-	}
+// async.waterfall([
+// 	function(callback) {
+// 		request(URL, function(err, response, body){
+// 			if(!err && response.statusCode == 200) {
+// 				var json = JSON.parse(body);
+// 				data.id = json[s_toSearch].id;
+// 				data.name = json[s_toSearch].name;
+// 				callback(null, data);
+// 			} else {
+// 				console.log(err);
+// 			}
+// 		});
+// 	},
+// ],
+// function(err,data) {
+// 	if(err) {
+// 		console.log(err);
+// 		return;
+// 	}
 
-	res.render('index', {
-		info: data
-	});
-});
+// 	res.render('./index');
+// });
 
 });
 
